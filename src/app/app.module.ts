beb6a8e583c7f0f51efe822662284bb99b401fb3
kapibarra2315 <nikita.nov2016@yandex.ru>
2022-10-11 02:28:49 +0300

    Added ng animate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -28,7 +29,8 @@ import { ContactsComponent } from './homepage/shared/components/contacts/contact
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/homepage/shared/components/about/about.component.ts
+++ src/app/homepage/shared/components/about/about.component.ts
@@ -1,8 +1,20 @@
+import { trigger, transition, style,animate,state} from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
+  animations: [
+    trigger('Opacity', [
+      transition(':enter', [
+        style({ opacity: 0}),
+        animate('350ms', style({ opacity: 1 })),
+      ]),
+      transition(':leave', [
+        animate('350ms', style({ opacity: 0 }))
+      ])
+    ]),
+  ],
   styleUrls: ['./about.component.scss']
 })
 export class AboutComponent implements OnInit {
