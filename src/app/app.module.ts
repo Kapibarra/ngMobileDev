35233ac5341fb7ec507b5e8f9c258937f288ce1a
kapibarra2315 <nikita.nov2016@yandex.ru>
2022-10-10 16:14:28 +0300

    init components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { HeaderComponent } from './homepage/shared/components/header/header.component';
+import { HeroComponent } from './homepage/shared/components/hero/hero.component';
+import { AboutComponent } from './homepage/shared/components/about/about.component';
+import { ProjectsComponent } from './homepage/shared/components/projects/projects.component';
+import { TrafficComponent } from './homepage/shared/components/traffic/traffic.component';
+import { MapComponent } from './homepage/shared/components/map/map.component';
+import { JobComponent } from './homepage/shared/components/job/job.component';
+import { ContactsComponent } from './homepage/shared/components/contacts/contacts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    HeaderComponent,
+    HeroComponent,
+    AboutComponent,
+    ProjectsComponent,
+    TrafficComponent,
+    MapComponent,
+    JobComponent,
+    ContactsComponent
   ],
   imports: [
     BrowserModule,
